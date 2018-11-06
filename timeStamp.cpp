a27ff4f9d7fdb4c10d59a2bd47d52a44d80b6ae9
Yashvi Verma <31646303+yashvi27@users.noreply.github.com>
2018-11-07 00:37:02 +0530

    Question 4

--- timeStamp.cpp
+++ timeStamp.cpp
@@ -5,15 +5,35 @@ using namespace std;
 
 bool isSubString(string s, string sb)
 {
-    int len, sublen, i, j=0;
+    int len, sublen, i=0, j=0, temp;
     len=s.length();
     sublen=sb.length();
 
-    for(i=0; i<len; i++)
+	while(i<len && j<sublen)
     {
-        if(sb[j]==s[i])  //character by character comparison
-            j++;
-    }
+			temp=j;
+			while(s[i] != ',' && i<len)
+			{
+				if (s[i] == sb[j])
+				{
+					i++;
+					j++;
+				}
+				else if (s[i] != sb[j])
+				{
+					j = temp;
+					i++;
+				}
+			}
+			if (s[i] == ',' && sb[j] == ',')
+			{
+
+				i++;
+				j++;
+			}
+			else
+				i++;
+	}
 
     if(j==sublen && j != 0) //length should not be zero
         return true;
@@ -25,9 +45,9 @@ int main()
     string str, sub;
     bool b;
 
-    cout<<"Enter the string: ";
+    cout<<"Enter the events(separated by comma): ";
     getline(cin,str);
-    cout<<"\nEnter the sub string: ";
+    cout<<"\nEnter the sub events(separated by comma): ";
     getline(cin,sub);
 
     b=isSubString(str, sub); //checks whether string entered is sub string is not
@@ -35,4 +55,8 @@ int main()
         cout<<"\nIt is a sub string.";
     else
         cout<<"\nNot a sub string.";
+
+    return 0;
 }
+
+
